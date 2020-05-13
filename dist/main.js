8f8ab0cf213a04e5fd65a5252ae20d893e92945c
Léa Buendé <46668755+leabuende@users.noreply.github.com>
2020-05-13 17:23:21 +0200

    add marker

--- dist/main.js
+++ dist/main.js
@@ -3,3 +3,8 @@ L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
     maxZoom: 20,
 }).addTo(map);
 setTimeout(function () { map.invalidateSize() }, 800);
+var marker = L.marker([48.8566969, 2.3514616]).addTo(map);
+var marker2 = L.marker([48.8616513, 2.3470129]).addTo(map);
+
+var popup = marker.bindPopup('<b>Lime</b><br />I am available!')
+var popup2 = marker2.bindPopup('<b>Lime</b><br />I am available!')
